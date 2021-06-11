c521992d088327d67eb47409fad2369098f4d27d
Harvey Appleton <info@harveyappleton.co.uk>
2021-06-11 16:46:17 +0100

    fix: Add 7th policy argument

--- src/casbinRule.ts
+++ src/casbinRule.ts
@@ -12,46 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
 @Entity()
 export class CasbinRule extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @PrimaryGeneratedColumn()
-    public id: number;
+  @Column({
+    nullable: true,
+  })
+  public ptype: string;
 
-    @Column({
-        nullable: true
-    })
-    public ptype: string;
+  @Column({
+    nullable: true,
+  })
+  public v0: string;
 
-    @Column({
-        nullable: true
-    })
-    public v0: string;
+  @Column({
+    nullable: true,
+  })
+  public v1: string;
 
-    @Column({
-        nullable: true
-    })
-    public v1: string;
+  @Column({
+    nullable: true,
+  })
+  public v2: string;
 
-    @Column({
-        nullable: true
-    })
-    public v2: string;
+  @Column({
+    nullable: true,
+  })
+  public v3: string;
 
-    @Column({
-        nullable: true
-    })
-    public v3: string;
+  @Column({
+    nullable: true,
+  })
+  public v4: string;
 
-    @Column({
-        nullable: true
-    })
-    public v4: string;
+  @Column({
+    nullable: true,
+  })
+  public v5: string;
 
-    @Column({
-        nullable: true
-    })
-    public v5: string;
+  @Column({
+    nullable: true,
+  })
+  public v6: string;
 }

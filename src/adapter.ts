77fbf0530725eb6004217e771aa9679f2d7eacc2
Hari Prasad <hariprasadiit@gmail.com>
2021-09-09 18:43:14 +0000

    fix: undfined tokens are becoming strings

--- src/adapter.ts
+++ src/adapter.ts
@@ -102,8 +102,8 @@ export default class TypeORMAdapter implements FilteredAdapter {
       line.ptype +
       ', ' +
       [line.v0, line.v1, line.v2, line.v3, line.v4, line.v5, line.v6]
-        .map((n) => `"${n}"`)
         .filter((n) => n)
+        .map((n) => `"${n}"`)
         .join(', ');
     Helper.loadPolicyLine(result, model);
   }

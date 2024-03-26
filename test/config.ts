f8d7ceb11900486fab79311b363f7becd153fdf5
jkalberer <jkalberer@audioeye.com>
2024-03-26 15:17:21 -0700

    Implement UpdatableAdapter on adapter
    This updates the adapter so that we can use `updatePolicy` from the enforcer.

--- test/config.ts
+++ test/config.ts
@@ -1,16 +1,28 @@
 import { DataSourceOptions } from 'typeorm';
 
-export const connectionConfig: DataSourceOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: parseInt(process.env.MYSQL_PORT || '', 10) || 3306,
-  username: process.env.MYSQL_USER || 'root',
-  password:
-    process.env.MYSQL_PASSWORD !== undefined
-      ? process.env.MYSQL_PASSWORD === ''
-        ? undefined
-        : process.env.MYSQL_PASSWORD
-      : 'password',
-  database: process.env.MYSQL_DB || 'casbin',
-  dropSchema: true,
-};
+const SHOULD_USE_MYSQL =
+  process.env.MYSQL_USER != null ||
+  process.env.MYSQL_PORT != null ||
+  process.env.MYSQL_PASSWORD != null ||
+  process.env.MYSQL_DB != null;
+
+export const connectionConfig: DataSourceOptions = SHOULD_USE_MYSQL
+  ? {
+      type: 'mysql',
+      host: 'localhost',
+      port: parseInt(process.env.MYSQL_PORT || '', 10) || 3306,
+      username: process.env.MYSQL_USER || 'root',
+      password:
+        process.env.MYSQL_PASSWORD !== undefined
+          ? process.env.MYSQL_PASSWORD === ''
+            ? undefined
+            : process.env.MYSQL_PASSWORD
+          : 'password',
+      database: process.env.MYSQL_DB || 'casbin',
+      dropSchema: true,
+    }
+  : {
+      type: 'sqlite',
+      database: ':memory:',
+      dropSchema: true,
+    };
